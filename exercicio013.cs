2475befc81f86478330ab8d2042e73f8389ad522
gusrocha26 <48576647+gusrocha26@users.noreply.github.com>
2020-04-05 16:28:41 -0300

    Exercício 013

---
+++ exercicio013.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Nota Final =" 
+                + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (aluno.Aprovado())
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Faltaram"
+                    + aluno.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)
+                    + "Pontos");
+                   
+            }
+
+
+        }
+    }
+}
---
+++ exercicioclasses013.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicio013
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double NotaFinal()
+        {
+            return  Nota1 + Nota2 + Nota3;
+        }
+        public double NotaRestante()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - NotaFinal();
+            }
+        }
+
+        public bool Aprovado()
+        {
+            if (NotaFinal() >= 60.00)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        
+    }
+}
