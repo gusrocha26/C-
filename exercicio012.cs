306e76e3ad0931556c914f009c0795025c9d5db7
gusrocha26 <48576647+gusrocha26@users.noreply.github.com>
2020-04-05 15:19:07 -0300

    Exercício 012

---
+++ exercicio012.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionarios dadosFunc = new Funcionarios();
+
+            Console.Write("Nome :");
+            dadosFunc.Nome = Console.ReadLine();
+            Console.Write("Salario Bruto: ");
+            dadosFunc.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Impostos:");
+            dadosFunc.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("Funcionarios:" + dadosFunc);
+
+            Console.WriteLine();
+            Console.WriteLine("Deseja aumentar o salario em qual porcentagem ?");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            dadosFunc.AumentarSalario(porcent);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + dadosFunc);
+
+
+
+
+
+
+
+            
+            
+
+        }
+    }
+}
