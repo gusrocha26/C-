a07f2907310f3d6cc1dbb7a5343dfe31d8b55fb0
gusrocha26 <48576647+gusrocha26@users.noreply.github.com>
2020-04-05 11:21:04 -0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace excclasses004
+{
+    class Pessoa
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Altura + Largura);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Altura * Altura + Largura * Largura);
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace excclasses004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+
+            Console.WriteLine("Entre a Largura e altura do retangulo: ");
+            p.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Area =" + p.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro = " + p.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = " + p.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+    
+        }
+    }
+}
