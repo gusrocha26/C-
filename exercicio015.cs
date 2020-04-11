41ff793c07dc4a601c411d9f86abb3e6bd554d15
gusrocha26 <48576647+gusrocha26@users.noreply.github.com>
2020-04-11 18:17:13 -0300

    Exercício 015
    Cadastro de quartos com vetores

---
+++ exercicio015.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercicio014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+
+            Console.Write("Entre com o número da conta:");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titula da conta:");
+            string titular = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial (s/n)?");
+            char resp = char.Parse(Console.ReadLine());
+            if(resp == 's' || resp== 'S')
+            {
+                Console.Write("Entre com valor de deposito inicial:");
+
+            }
+
+           
+
+           
+
+        }
+    }
+}
---
+++ exercicio015classe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicio014
+{
+    class ContaBancaria
+
+    {
+
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        
+        
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+
+
+        }
+
+        public ContaBancaria( int numero, string titular, double saldo) : this(numero, titular)
+        {
+            Saldo = saldo;
+        }
+
+
+
+
+
+
+
+    }
+}
